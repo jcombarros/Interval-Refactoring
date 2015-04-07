37b8646e7259b50ba008baef3c822051583d7e4d
Javier Combarros <Javier@138.4.192.251>
2015-04-07 22:06:39 +0200

    Make abstract Interval class. Make abstract include(value) method

--- src/intervals/Interval.java
+++ src/intervals/Interval.java
@@ -1,6 +1,6 @@
 package intervals;
 
-public class Interval {
+public abstract class Interval {
  
 	protected double minimum;
 	protected double maximum;
@@ -39,21 +39,7 @@ public class Interval {
 		return (maximum + minimum) / 2;
 	}
 
-	public boolean includes(double value) {
-		switch (opening) {
-		case BOTH_OPENED:
-			return minimum < value && value < maximum;
-		case LEFT_OPENED:
-			return minimum < value && value <= maximum;
-		case RIGHT_OPENED:
-			return minimum <= value && value < maximum;
-		case UNOPENED:
-			return minimum <= value && value <= maximum;
-		default:
-			assert false;
-			return false;
-		}
-	}
+	public abstract boolean includes(double value) ;
 
 	public boolean includes(Interval interval) {
 		boolean minimumIncluded = this.includes(interval.minimum);
