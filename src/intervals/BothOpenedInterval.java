edd237b07f047a1c0527c03b41696d7991c40e92
Javier Combarros <Javier@138.4.192.251>
2015-04-07 22:10:57 +0200

    Declare method includes(interval) in all subclasses

--- src/intervals/BothOpenedInterval.java
+++ src/intervals/BothOpenedInterval.java
@@ -13,4 +13,26 @@ public class BothOpenedInterval extends Interval {
 	public boolean includes(double value) {
 		return minimum < value && value < maximum;
 	}
+	
+	public boolean includes(Interval interval) {
+		boolean minimumIncluded = this.includes(interval.minimum);
+		boolean maximumIncluded = this.includes(interval.maximum);
+
+		switch (interval.opening) {
+			case BOTH_OPENED:
+				return (minimumIncluded || minimum == interval.minimum)
+						&& (maximumIncluded || maximum == interval.maximum);
+			case LEFT_OPENED:
+				return (minimumIncluded || minimum == interval.minimum)
+						&& (maximumIncluded);
+			case RIGHT_OPENED:
+				return (minimumIncluded)
+						&& (maximumIncluded || maximum == interval.maximum);
+			case UNOPENED:
+				return (minimumIncluded) && (maximumIncluded);
+			default:
+				assert false;
+				return false;
+		}
+	}
 }
--- src/intervals/RightOpenedInterval.java
+++ src/intervals/RightOpenedInterval.java
@@ -13,4 +13,27 @@ public class RightOpenedInterval extends Interval {
 	public boolean includes(double value) {
 		return minimum <= value && value < maximum;
 	}
+	
+	public boolean includes(Interval interval) {
+		boolean minimumIncluded = this.includes(interval.minimum);
+		boolean maximumIncluded = this.includes(interval.maximum);
+
+		switch (interval.opening) {
+			case BOTH_OPENED:
+				return (minimumIncluded || minimum == interval.minimum)
+						&& (maximumIncluded || maximum == interval.maximum);
+			case LEFT_OPENED:
+				return (minimumIncluded || minimum == interval.minimum)
+						&& (maximumIncluded);
+			case RIGHT_OPENED:
+				return (minimumIncluded || minimum == interval.minimum)
+						&& (maximumIncluded || maximum == interval.maximum);
+			case UNOPENED:
+				return (minimumIncluded || minimum == interval.minimum)
+						&& (maximumIncluded);
+			default:
+				assert false;
+				return false;
+		}
+	}
 }
