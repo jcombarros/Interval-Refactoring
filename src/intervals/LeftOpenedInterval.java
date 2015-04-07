02206859da5c5d3a2a38613f8a2a1a5e3f05da25
Javier Combarros <Javier@138.4.192.251>
2015-04-07 23:40:31 +0200

    Implement includesResult(...) methods in all the subclasses

--- src/intervals/LeftOpenedInterval.java
+++ src/intervals/LeftOpenedInterval.java
@@ -37,6 +37,30 @@ public class LeftOpenedInterval extends Interval {
 		}	
 	}
 	
+	protected boolean includesResult(Interval interval, boolean minimumIncluded, boolean maximumIncluded){
+		return interval.includesResult(this, minimumIncluded, maximumIncluded);
+	}
+
+	protected boolean includesResult(LeftOpenedInterval interval, boolean minimumIncluded, boolean maximumIncluded){
+		return (minimumIncluded || minimum == interval.minimum)
+				&& (maximumIncluded || maximum == interval.maximum);
+	}
+
+	protected boolean includesResult(RightOpenedInterval interval, boolean minimumIncluded, boolean maximumIncluded){
+		return (minimumIncluded || minimum == interval.minimum)
+				&& (maximumIncluded);
+	}
+	
+	protected boolean includesResult(BothOpenedInterval interval, boolean minimumIncluded, boolean maximumIncluded){
+		return (minimumIncluded || minimum == interval.minimum)
+				&& (maximumIncluded);
+	}
+	
+	protected boolean includesResult(UnopenedInterval interval, boolean minimumIncluded, boolean maximumIncluded){
+		return (minimumIncluded || minimum == interval.minimum)
+				&& (maximumIncluded || maximum == interval.maximum);
+	}
+	
 	public boolean intersectsWith(Interval interval) {
 		if (minimum == interval.maximum) {
 			return false;
