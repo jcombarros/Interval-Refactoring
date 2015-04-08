686278f1d535a0ed2be19317efe34ebdc2dde5fe
Javier Combarros <Javier@138.4.192.251>
2015-04-08 22:45:45 +0200

    Point class created. Property and Constructor added

---
+++ src/intervals/Point.java
@@ -0,0 +1,19 @@
+package intervals;
+
+public class Point {
+	
+	private double value;
+	
+	public Point (double value){
+		this.value = value;
+	}
+	
+	public double getValue(){
+		return this.value;
+	}
+	
+	public void setValue(double value){
+		this.value = value;
+	}
+
+}
